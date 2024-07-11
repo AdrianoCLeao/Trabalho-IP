73490febd253b1034e8d9405f31fb287c64fd1c3
Miguel Bertol <miguelbertolcodes@gmail.com>
2024-07-11 09:36:47 -0300

    feat: add fake clients database functions

---
+++ src/clientes/clientes.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+FILE* criarBancoClientes() {
+    FILE *bancoClientes = fopen("clientes.txt", "r");
+
+    if (bancoClientes == NULL) {
+        printf("Tentando criar novo banco de clientes...\n");
+        bancoClientes = fopen("clientes.txt", "w");
+        if (bancoClientes == NULL) {
+            printf("Erro ao criar um banco de clientes\n");
+            return NULL;
+        } else {
+            rewind(bancoClientes);
+            fputs("Banco pronto para receber dados", bancoClientes);
+            fclose(bancoClientes);
+            printf("Banco de clientes criado\n");
+            return bancoClientes;
+        }
+    } else {
+        return bancoClientes;
+    }
+}
+
+FILE* conectarBancoClientes() {
+    FILE *bancoClientes = fopen("clientes.txt", "r");
+
+    if (bancoClientes == NULL) {
+        printf("Erro ao conectar com o banco de clientes\n");        
+        bancoClientes = criarBancoClientes();
+        if (bancoClientes == NULL) {
+            printf("Nao foi possivel se conectar ao banco de clientes\n");
+            return(NULL);
+        } else {
+            printf("Conectado ao novo banco de clientes\n");
+            return bancoClientes;
+        }
+    } else {
+        printf("Conectado ao banco de clientes\n");
+        return bancoClientes;
+    }
+}
---
+++ src/clientes/clientes.h
@@ -0,0 +1,2 @@
+FILE* criarBancoClientes();
+FILE* conectarBancoClientes();
